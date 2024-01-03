07b04ad1a4ac45d69dce34c14b14eaebc7587307
Ashwini Bhide <60039504+slytherin007@users.noreply.github.com>
2024-01-03 23:58:38 +0530

    Add files via upload

---
+++ Scaffold/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor:Colors.lightBlueAccent,
+        body:Center(
+        child: Container(
+          height:300,
+          width:300,
+          color: Colors.lightGreenAccent,
+        ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+
+    );
+  }
+}
