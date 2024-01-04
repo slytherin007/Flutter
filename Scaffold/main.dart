4fbce7d46d653aa2a2bcb60c0161918154f3bfcc
Ashwini Bhide <60039504+slytherin007@users.noreply.github.com>
2024-01-04 23:46:38 +0530

    Update main.dart

--- Scaffold/main.dart
+++ Scaffold/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor:Colors.lightBlueAccent,
         body:Center(
